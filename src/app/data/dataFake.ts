02ed7aec759a5ba6ae50b9a61ea5300d5410f59b
Maurício Santos <mauricioalsantos@gmail.com>
2024-03-19 15:57:21 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [{
+    "id": 1, "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkPe9pNfrShEJZd-9vbLg3pzW7wQuea_pLxg&usqp=CAU",
+    "title": "Novo Homem de Ferro Anunciado", "description": "Novo filme do homem de ferro chegando aos cinemas em 2024."
+},
+{
+    "id": 2, "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3q-Iry70s0bKQJoy69QFeS56YPBA0rnPgyw&usqp=CAU",
+    "title": "Nova Série Disney", "description": "Nova série Disney chegando ao Disney+ em 2024."
+},
+{
+    "id": 3, "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQommmajwbAZtB2T-f8rx62RbDyz65d54uAZw&usqp=CAU",
+    "title": "Nova Série Disney", "description": "Nova série Disney chegando ao Disney+ em 2024."
+},
+{
+    "id": 4, "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSZo7IqEUYLTbsC_Y63EgDDYDyG-FdTiPddw&usqp=CAU",
+    "title": "Nova Série Disney", "description": "Nova série Disney chegando ao Disney+ em 2024."
+}
+]
